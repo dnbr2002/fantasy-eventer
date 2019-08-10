31124791970d21bec007a6b12cfb15b2cd7b6172
Puerto <dnbr2002@yahoo.com>
2019-08-10 15:55:10 -0400

    updated contact page

--- src/views/pages/contact/contact.js
+++ src/views/pages/contact/contact.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 import classNames from 'classnames';
 // Material helpers
-import { withStyles } from '@material-ui/core';
-//import PropTypes from 'prop-types'
+import { withStyles, Typography, LinearProgress, Paper, Divider } from '@material-ui/core';
+
+// Icons
+import {
+EmailOutlined
+} from '@material-ui/icons';
+
+// Shared components
+import Portlet from '../../components/Portlet';
+import PortletContent from '../../components/PortletContent';
+import PortletFooter from '../../components/PortletFooter';
 
 // Component styles
 const styles = theme => ({
     root: {
       padding: theme.spacing(4)
-    }
+    },
+    email: {
+        textAlign: 'center'
+    },
+    twitter: {
+        textAlign: 'center'
+    },
   });
 
  class ContactPage extends Component {
@@ -17,15 +32,24 @@ const styles = theme => ({
         const rootClassName = classNames(classes.root, className);
         return (
             <div className={rootClassName}>
-            <div className="g-row">
-                <div className="g-col">
-                <div className="bootstrap">
-                <div className="jumbotron">
-                    <h1 className="display-3">Contact</h1>
-                    </div>
-                    </div>
-                </div>
-            </div>
+        <Portlet className={classes.root} >
+            <PortletContent >
+                    <div>
+                    <Typography variant="h2" color="textSecondary" className={classes.email}>Contacts</Typography>
+                    <br /> 
+                        <br />
+                        <div className={classes.email}><EmailOutlined>Email</EmailOutlined>
+                        {/* <Typography variant="h3" color="textSecondary" className={classes.email}>Email</Typography> */}
+                        </div>
+                        <br /> 
+                        <br />
+                        <Divider />
+                        <br /> 
+                        <br />
+                        <Typography variant="h3" color="textSecondary"className={classes.twitter}>Twitter</Typography>
+                    </div>                    
+            </PortletContent>
+        </Portlet>
             </div>
         )
     }
